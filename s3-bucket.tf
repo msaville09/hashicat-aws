d7fc6466689696968dbaabfe6b8f03255695fe6f
root <michael.saville@lmco.com>
2022-06-23 20:29:58 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,9 +1,10 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
+  bucket_prefix = "msaville"
   bucket = "Gaurav-bucket"
   acl    = "private"
-  bucket_prefix = "msaville"
+
   versioning = {
     enabled = true
   }
